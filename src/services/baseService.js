e990c2a5a787cf241b25c5b7f181f51cc6684ce7
hnquocanh10a4 <hnquocanh10a4@gmail.com>
2023-05-26 11:10:42 +0700

    update be url

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -1,40 +1,55 @@
-import Axios from "axios"
-import { DOMAIN, TOKEN } from "../until/setting/configs"
+import Axios from 'axios';
+import { DOMAIN, TOKEN } from '../until/setting/configs';
 
 export class baseService {
     //put json về phía backend
-    put = (url,model) => {
-        return  Axios({
-            url:`${DOMAIN}/${url}`,
-            method:'PUT',
-            data:model,
-            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //JWT
-        }) 
-    }
-
-    post = (url,model) => {
+    put = (url, model) => {
         return Axios({
-            url:`${DOMAIN}/${url}`,
-            method:'POST',
-            data:model,
-            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //JWT
-        }) 
-    }
+            url: `${DOMAIN}/${url}`,
+            method: 'PUT',
+            data: model,
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem(TOKEN),
+                TokenCybersoft:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MSIsIkhldEhhblN0cmluZyI6IjEyLzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NDQ3NjgwMDAwMCIsIm5iZiI6MTY2NTI0ODQwMCwiZXhwIjoxNjk0NjI0NDAwfQ.SUELcPShU58ZkNS3CbFDhM02KMzll9j00ndjVSaiJ8Q',
+            }, //JWT
+        });
+    };
 
+    post = (url, model) => {
+        return Axios({
+            url: `${DOMAIN}/${url}`,
+            method: 'POST',
+            data: model,
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem(TOKEN),
+                TokenCybersoft:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MSIsIkhldEhhblN0cmluZyI6IjEyLzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NDQ3NjgwMDAwMCIsIm5iZiI6MTY2NTI0ODQwMCwiZXhwIjoxNjk0NjI0NDAwfQ.SUELcPShU58ZkNS3CbFDhM02KMzll9j00ndjVSaiJ8Q',
+            }, //JWT
+        });
+    };
 
     get = (url) => {
         return Axios({
-            url:`${DOMAIN}/${url}`,
-            method:'GET',
-            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
-        })
-    }
+            url: `${DOMAIN}/${url}`,
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem(TOKEN),
+                TokenCybersoft:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MSIsIkhldEhhblN0cmluZyI6IjEyLzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NDQ3NjgwMDAwMCIsIm5iZiI6MTY2NTI0ODQwMCwiZXhwIjoxNjk0NjI0NDAwfQ.SUELcPShU58ZkNS3CbFDhM02KMzll9j00ndjVSaiJ8Q',
+            }, //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
+        });
+    };
 
     delete = (url) => {
         return Axios({
-            url:`${DOMAIN}/${url}`,
-            method:'DELETE',
-            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
-        })
-    }
+            url: `${DOMAIN}/${url}`,
+            method: 'DELETE',
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem(TOKEN),
+                TokenCybersoft:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0MSIsIkhldEhhblN0cmluZyI6IjEyLzA5LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5NDQ3NjgwMDAwMCIsIm5iZiI6MTY2NTI0ODQwMCwiZXhwIjoxNjk0NjI0NDAwfQ.SUELcPShU58ZkNS3CbFDhM02KMzll9j00ndjVSaiJ8Q',
+            }, //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
+        });
+    };
 }
--- src/until/setting/configs.js
+++ src/until/setting/configs.js
@@ -1,4 +1,4 @@
-export const DOMAIN = 'https://movieapi.cyberlearn.vn';
+export const DOMAIN = 'https://movienew.cybersoft.edu.vn';
 export const TOKEN = 'ACCESS_TOKEN';
 export const GROUPID = 'GP04';
 export const USER_LOGIN = 'USER_LOGIN';
